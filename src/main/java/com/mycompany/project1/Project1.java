b459463273bda04fa1ce313bce41c0c4edd758ce
Lenovo <Lenovo@DESKTOP-VCJBTUR>
2024-01-07 23:44:07 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/project1/Project1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Project1 {
+
+    private String name;
+    private int x;
+    private int y;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
